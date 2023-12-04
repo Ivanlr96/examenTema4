54adf3eda3a32a5a43cff3baf2a8ddbf68d095b1
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-12-04 09:04:48 +0100

    Creado el método para mostrar datos

--- scripts/main.js
+++ scripts/main.js
@@ -22,4 +22,14 @@ class Vehiculo {
         this.#fabricacion=fabricacion;
         this.#cilindrada=cilindrada;
     }
+    mostrarDatos() {
+        const datos=`
+        <p> La marca del vehículo  es -> ${this.#marca}</p>
+        <p> El modelo del vehículo es -> ${this.#modelo}</p>
+        <p> El color del vehículo es -> ${this.#color}</p>
+        <p> El vehiculo se fabricó en -> ${this.#fabricacion}</p>
+        <p> El vehiculo tiene una cilindrada de -> ${this.#cilindrada}</p>
+        `
+        mostrarResultado()=`<p>${datos}</p>`
+    }
 }
